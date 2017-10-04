7b34708177339838331f45b6404adcdbf4a36163
Galliez Bruno <galliez.bruno@gmail.com>
2017-10-04 14:36:04 +0200

    Java, à corriger et finir

--- Android/app01/app/src/main/java/com/damocorop/app01/Activity01.java
+++ Android/app01/app/src/main/java/com/damocorop/app01/Activity01.java
@@ -1,9 +1,13 @@
 package com.damocorop.app01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -12,11 +16,33 @@ public class Activity01 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_01);
+        setContentView(R.layout.start);
 
-        EditText nom = (EditText) findViewById(R.id.txtNom);
-        TextView resultat = (TextView) findViewById(R.id.txtResultat);
-        resultat.setText( nom.getText() );
+        //EditText nom = (EditText) findViewById(R.id.txtNom);
+        //TextView resultat = (TextView) findViewById(R.id.txtResultat);
+        //resultat.setText( nom.getText() );
+
+        // attacher une fonction à un évènement ( même style que javascript )
+        Button bouton5 = (Button) findViewById(R.id.btStartEnvoyer);
+        bouton5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"test",Toast.LENGTH_SHORT);
+                // appeler une nouvelle activity
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                // startActivity est déclaré ans AppCompatActivity
+                //MainActivity.this si nécessaire
+                startArctivity(intent);
+            }
+        });
+
     }
+
+    // remplacé par le code au dessus pour l'attacher à l'évènement d'un bouton
+    //public void onClickOnButton(View v){
+
+    //    Toast.makeText(this,"clicage de butonnage",Toast.LENGTH_SHORT);
+
+    //}
 
 }
---
+++ Android/app01/app/src/main/java/com/damocorop/app01/Prompt.java
@@ -0,0 +1,18 @@
+package com.damocorop.app01;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by super on 04/10/2017.
+ */
+
+public class Prompt extends AppCompatActivity {
+
+    public static final int TEST_INT_CONSTANT = 1000;
+
+    @Override
+    protected void Oncreate(){
+
+    }
+
+}
