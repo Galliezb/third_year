c800407a1eaa6f44ebc3f05e1b3170d7bacdb6e9
Galliez Bruno <galliez.bruno@gmail.com>
2017-10-11 11:35:19 +0200

    Starting with Jquery

---
+++ progra web/WebApplicationNetFramework/Jquery02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jquery02.MvcApplication" Language="C#" %>
---
+++ progra web/WebApplicationNetFramework/Jquery02/Scripts/js.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function(){
+
+    // pour éviter les soucis de visibility VS display ( height box )
+    // on cache les textes depuis le JS
+    // il reste le bon vieux display none en CSS
+    // Jquery gère très bien ensuite via le slide
+    //$("#menuDepliant").find("div").each(function(){
+        //$(this).hide();
+    //});
+
+    // toggleslide sur chaque div présent dans le parent
+    // et on dépli forcément celui en dessous du click
+    $("#menuDepliant").on("click","h1" , function(){
+        
+        $("#menuDepliant").find("div").each(function(){
+            $(this).slideUp("slow");
+        });
+        $(this).next().slideToggle("slow");
+
+    });
+
+});
