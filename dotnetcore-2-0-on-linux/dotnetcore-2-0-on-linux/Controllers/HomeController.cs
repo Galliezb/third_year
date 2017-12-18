5f5a196a01b067f1a29458e9aa7b790e04170a41
Galliez Bruno <galliez.bruno@gmail.com>
2017-12-18 13:35:21 +0100

    test dotnetcore sur VM

---
+++ dotnetcore-2-0-on-linux/dotnetcore-2-0-on-linux/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_2_0_on_linux.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
